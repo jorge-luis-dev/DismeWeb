2a5204215ba6e6de5449f37572fd13383ded93ca
Jorge Luis <jorgequiguango@outlook.com>
2024-12-12 22:30:08 -0500

    [IMP] Dependencies in manifest web portal module

--- web_portal/__manifest__.py
+++ web_portal/__manifest__.py
@@ -3,12 +3,18 @@
     "summary": "Website for Disme",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "author": "Jorge Luis,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-ecuador",
     "category": "website",
-    "depends": ["portal"],
+    "depends": [
+        "stock",
+        "portal",
+        "product_categ_image",
+        "product_category_custom",
+    ],
     "data": [
         # 'security/ir.model.access.csv',
         "views/home_web_portal.xml",
     ],
+    "installable": True,
 }
