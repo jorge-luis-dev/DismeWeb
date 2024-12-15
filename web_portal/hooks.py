564b0edc9f0df94fdd2250c5d84472ce0bd49fa5
Jorge Luis <jorjoluiso@gmail.com>
2024-12-15 11:10:10 -0500

    [REF] Added design cards in home page

--- web_portal/hooks.py
+++ web_portal/hooks.py
@@ -1,8 +1,7 @@
-
 def post_init_hook(env):
     all_companies = env["res.company"].search([])
     country_code = "EC"
     country = env["res.country"].search([("code", "=", country_code)])
-    
+
     for company in all_companies:
         company.sudo().write({"country_id": country.id})
