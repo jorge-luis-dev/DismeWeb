6874a8591571836441c47efbc1942771abc8f0bc
Jorge Luis <jorjoluiso@gmail.com>
2024-12-16 10:35:43 -0500

    [I] Added 404 message

---
+++ web_portal/controllers/categorie.py
@@ -0,0 +1,35 @@
+from odoo.http import request, route
+
+from odoo.addons.website.controllers.main import Home
+
+
+class Categorie(Home):
+    @route(
+        "/hola/odoo/v1/products/category/<int:category_id>", auth="public", website=True
+    )
+    def products_category(self, category_id, **kwargs):
+        result = (
+            request.env["product.category"]
+            .sudo()
+            .search_read(
+                [("id", "=", category_id), ("is_published", "=", True)],
+                ["name"],
+                limit=1,
+            )
+        )
+
+        if result:
+            category_name = f"Products of {result[0]["name"]}"
+
+            Product = request.env["product.template"]
+            products = Product.sudo().search([("categ_id", "=", category_id)])
+        else:
+            return request.render("web_portal.404", status=404)
+
+        return request.render(
+            "web_portal.products_category_web_portal",
+            {
+                "products": products,
+                "categorie_name": category_name,
+            },
+        )
--- web_portal/controllers/main.py
+++ web_portal/controllers/main.py
@@ -17,34 +17,3 @@ class Main(Home):
         )
 
         return res
-
-    @route(
-        "/hola/odoo/v1/products/category/<int:category_id>", auth="public", website=True
-    )
-    def products_category(self, category_id, **kwargs):
-        result = (
-            request.env["product.category"]
-            .sudo()
-            .search_read(
-                [("id", "=", category_id), ("is_published", "=", True)],
-                ["name"],
-                limit=1,
-            )
-        )
-
-        if result:
-            categorie_name = f"Productos de {result[0]["name"]}"
-
-            Product = request.env["product.template"]
-            products = Product.sudo().search([("categ_id", "=", category_id)])
-        else:
-            categorie_name = "Productos no encontrados"
-            products = False
-
-        return request.render(
-            "web_portal.products_category_web_portal",
-            {
-                "products": products,
-                "categorie_name": categorie_name,
-            },
-        )
